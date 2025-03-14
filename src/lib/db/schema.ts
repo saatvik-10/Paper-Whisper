b4d9411de76c8ef8d8bcd791032b78a9a5868f20
saatvik-10 <saatvikmadan529@gmail.com>
2025-03-14 18:13:37 +0530

    added drizzle-orm

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,30 @@
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from 'drizzle-orm/pg-core';
+
+export const userSystemEnum = pgEnum('user_system_enum', ['system', 'user']);
+
+export const chats = pgTable('chats', {
+  id: serial('id').primaryKey(),
+  pdfName: text('pdf_name').notNull(),
+  pdfUrl: text('pdf_url').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  userId: varchar('user_id', { length: 255 }).notNull(), //pointing to clerk user id
+  fileKey: text('file_key').notNull(), //name of file retreived from s3
+});
+
+export const messages = pgTable('messages', {
+  id: serial('id').primaryKey(),
+  chatId: integer('chat_id')
+    .references(() => chats.id)
+    .notNull(), //pointing to chat id
+  content: text('content').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  role: userSystemEnum('role').notNull(),
+});
